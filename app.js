7c6a5574dd6e07178008e4ac69f5391798416c48
surender18 <surenderempire68@gmail.com>
2025-09-03 17:47:15 +0530

    add seperation of concerns in models and created owner model.

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express= require("express")
 const app= express()
+const cookieParser= require('cookie-parser')
+const path = require('path')
+const db=require('./config/mongooseConnection')
+const adminRouter=require('./routes/adminRouter')
+const productRouter=require('./routes/ProductRouter')
+const userRouter=require('./routes/userRouter')
 
-app.get("/", (req, res) => {
-  res.send("Hello User!")
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+  res.send("Welcome to LuxCarry")
 })
+app.use('/admin',adminRouter)
+app.use('/product',productRouter)
+app.use('/user',userRouter)
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
